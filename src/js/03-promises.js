be8e75e7327fac60e9638bbc9edf3f4a81393510
Anastasiia <pavlishinka12@icloud.com>
2022-10-20 22:23:39 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,26 +1,35 @@
 import { Notify } from 'notiflix';
 const form = document.querySelector('.form');
 
-const object = []
-
-form.addEventListener('input', e => {
-  object[e.target.name] = e.target.value;
-});
-
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
-
-
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  } else {
-    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+form.addEventListener('submit', sendForm);
+function sendForm(event) {
+  const firstDelay = Number(document.querySelector('[name="delay"]').value);
+  const delayStep = Number(document.querySelector('[name="step"]').value);
+  const amount = document.querySelector('[name="amount"]');
+  // debugger;
+  let step = firstDelay;
+  event.preventDefault();
+  for (let index = 1; index <= amount.value; index++) {
+    createPromise(index, step)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+      step += delayStep;
   }
 }
